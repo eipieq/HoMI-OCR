2e74026a56b457f15d9aeecb01fe2269a5540c4d
Anil <a2@sandpark.co (github.com)>
2023-12-12 12:15:20 +0530

    🚀 feat: Add auto-loop functionality to extract OCR results from multiple pages
    - Create a new file `auto-loop.py` to automate the process of extracting OCR results from multiple pages.
    - Use the Selenium WebDriver to interact with the OCR service page.
    - Loop through 3 pages and perform the following steps on each page:
      - Click the hidden file input to upload an image file.
      - Wait for the file input to become available and send the file path.
      - Wait for some time to let the OCR process the image.
      - Switch to the iframe containing the result.
      - Get the text from the `<p>` tag which contains the result.
      - Switch back to the main document.
      - Write the result text to a file.
    - Close the browser after processing each page.
    - This new functionality enhances the automation of the OCR process and allows extraction of OCR results from multiple pages.

---
+++ auto-loop.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# Set up the Selenium WebDriver with the service
+service = Service('/Users/zap/Documents/GitHub/sanskritocr/geckodriver')
+
+# Loop for 3 pages
+for page_number in range(1, 4):
+    driver = webdriver.Firefox(service=service)
+
+    try:
+        # Open the OCR service page
+        driver.get('https://ocr.sanskritdictionary.com')
+
+        # Use JavaScript to click the hidden file input
+        driver.execute_script("document.getElementById('pictureFile').click();")
+
+        # Wait for the file input to become available and send the file path
+        wait = WebDriverWait(driver, 10)
+        file_input = wait.until(EC.presence_of_element_located((By.ID, 'pictureFile')))
+        file_input.send_keys(f'/Users/zap/Documents/GitHub/sanskritocr/image{page_number}.jpg')
+
+        # Wait for some time to let the OCR process the image
+        # In a real-world scenario, you should wait for a specific element that indicates the process is complete
+        time.sleep(15)
+
+        # The result is within an iframe, so we switch to the iframe
+        iframe = driver.find_element(By.CSS_SELECTOR, '#tinymcetext_ifr')
+        driver.switch_to.frame(iframe)
+
+        # Now get the text from the <p> tag which contains the result
+        result_text = driver.find_element(By.TAG_NAME, 'p').text
+
+        # Switch back to the main document
+        driver.switch_to.default_content()
+
+        # Write the result text to a file
+        with open(f'/Users/zap/Documents/GitHub/sanskritocr/ocr_result{page_number}.txt', 'w') as file:
+            file.write(result_text)
+
+    finally:
+        # Close the browser
+        driver.quit()
