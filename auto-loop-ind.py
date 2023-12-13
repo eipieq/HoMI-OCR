c8f93bef18ee927a24057efaeefff98e501f47de
Anil <a2@sandpark.co (github.com)>
2023-12-13 12:22:52 +0530

    🚀 feat: Automate OCR Process for Multiple Pages
    - Add a new script `auto-loop-ind.py` to automate the OCR process for multiple pages.
    - This script uses Selenium WebDriver with Firefox to interact with the OCR service page.
    - The script loops through 3 pages and performs the following steps for each page:
      - Clicks the hidden file input to select the image file.
      - Waits for the file input to become available and sends the image file path.
      - Waits for the OCR process to complete.
      - Retrieves the result text from the page.
      - Writes the result text to a file.
    - The `auto-loop-ind.py` script is platform-independent and dynamically selects the appropriate geckodriver executable based on the operating system.
    - Update the existing script `auto-loop.py` to use the new geckodriver executable path.
    - Update the file paths in `auto-loop.py` to use a relative path for the image files and result text files.
    - Add new geckodriver executable files for macOS (`geckodriver_mac`) and Windows (`geckodriver_windows.exe`).
    - Add two new image files (`image2.jpg` and `image3.jpg`) for testing purposes.

--- auto-loop-ind.py
+++ auto-loop-ind.py
@@ -5,9 +5,26 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import os
+import platform
 
-# Set up the Selenium WebDriver with the service
-service = Service('/Users/zap/Documents/Work/CCL/HoMI-OCR/geckodriver')
+# Get the current working directory
+current_directory = os.getcwd()
+
+# Determine the operating system
+system = platform.system()
+
+# Set up the Selenium WebDriver with the appropriate geckodriver executable
+if system == 'Darwin':
+    driver_path = os.path.join(current_directory, 'geckodriver_mac')
+elif system == 'Linux':
+    driver_path = os.path.join(current_directory, 'geckodriver_linux')
+elif system == 'Windows':
+    driver_path = os.path.join(current_directory, 'geckodriver_windows.exe')
+else:
+    raise Exception(f"Unsupported operating system: {system}")
+
+service = Service(driver_path)
 
 # Loop for 3 pages
 for page_number in range(1, 4):
@@ -23,7 +40,11 @@ for page_number in range(1, 4):
         # Wait for the file input to become available and send the file path
         wait = WebDriverWait(driver, 10)
         file_input = wait.until(EC.presence_of_element_located((By.ID, 'pictureFile')))
-        file_input.send_keys(f'/Users/zap/Documents/Work/CCL/HoMI-OCR/image{page_number}.jpg')
+        # file_input.send_keys(f'/Users/zap/Documents/Work/CCL/HoMI-OCR/image{page_number}.jpg')
+        
+        # Use a relative path for the image file
+        image_path = os.path.join(current_directory, f'image{page_number}.jpg')
+        file_input.send_keys(image_path)
 
         # Wait for some time to let the OCR process the image
         # In a real-world scenario, you should wait for a specific element that indicates the process is complete
@@ -40,7 +61,9 @@ for page_number in range(1, 4):
         driver.switch_to.default_content()
 
         # Write the result text to a file
-        with open(f'/Users/zap/Documents/Work/CCL/HoMI-OCR/ocr_result{page_number}.txt', 'w') as file:
+        # Use a relative path for the result text file
+        result_file_path = os.path.join(current_directory, f'ocr_result{page_number}.txt')
+        with open(result_file_path, 'w') as file:
             file.write(result_text)
 
     finally:
